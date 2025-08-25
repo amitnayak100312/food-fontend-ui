6715bb399fbfa15b2b74dec7a68734420c4c3016
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-25 19:06:04 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantCard.jsx
+++ src/component/Restaurant/RestaurantCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const RestaurantCard = () => {
   return (
     <Card className='m-5 w-[18rem]'>
-            <div className='cursor-pointer'>
-                
+            <div className='{cursor-pointer}'>
+
             </div>
           </Card>
   )
