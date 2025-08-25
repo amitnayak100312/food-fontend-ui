1c050dae0972e03581987da6ffebc5316a44cb87
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-25 19:05:04 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantCard.jsx
+++ src/component/Restaurant/RestaurantCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RestaurantCard = () => {
   return (
     <Card className='m-5 w-[18rem]'>
-            <div className='cu'></div>
+            <div className='cursor'></div>
           </Card>
   )
 }
