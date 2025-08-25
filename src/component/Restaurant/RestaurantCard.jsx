ffb1d1d51f69b8ab9eb0c793aaa26cf2c3f02de9
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-25 19:09:04 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantCard.jsx
+++ src/component/Restaurant/RestaurantCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RestaurantCard = () => {
   return (
     <Card className='m-5 w-[18rem]'>
-            <div className={`${true?' cursor-pointer':}`}>
+            <div className={`${true?' cursor-pointer':"cursor-not-allowed"}relative`}>
 
             </div>
           </Card>
