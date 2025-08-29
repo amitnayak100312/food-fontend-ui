c403ada25f31ee7ac5476d847d75d63de51af397
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-29 22:24:46 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantCard.jsx
+++ src/component/Restaurant/RestaurantCard.jsx
@@ -17,8 +17,9 @@ const RestaurantCard = () => {
 
             <div className='p-4 textPart lg:flex w-full justify-between'>
                 <div className='space-y-1'>
+                  <p>Indian Fast Food</p>
                     <p className='text-sm'>
-                        Cravring it all? Dive into our global fla...
+                        Craving it all? Dive into our global flavors and satisfy your taste buds with a variety of cuisines from around the world.
                     </p>
                 </div>
             </div>
