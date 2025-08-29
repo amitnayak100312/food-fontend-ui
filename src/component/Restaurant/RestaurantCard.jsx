c183fc52fefbb5ff4880b3759fb491c208423a91
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-29 22:35:46 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantCard.jsx
+++ src/component/Restaurant/RestaurantCard.jsx
@@ -9,7 +9,7 @@ const RestaurantCard = () => {
             <div className={`${true?' cursor-pointer':"cursor-not-allowed"}relative`}>
                     <img 
                     className='w-full h-[10rem] object-cover rounded-t-md'
-                    src="https://via.placeholder.com/150" alt="" />
+                    src="" alt="" />
                 <Chip 
                 size="small"
                 className="absolute top-2 left-2"
