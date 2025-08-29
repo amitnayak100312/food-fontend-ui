07cc70e1e54fa8e690a17832db804d19f5cbeebf
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-29 23:06:46 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantDetail.jsx
+++ src/component/Restaurant/RestaurantDetail.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const RestaurantDetail = () => {
   return (
-    <div className='px-'>
-        
+    <div className='px-5 lg:px-20'>
+        <sectiosec          <h2 className='text-2xl font-semibold'>Restaurant Name</h2>
+          <p className='text-sm text-gray-600'>Cuisine: Indian</p>
+          <p className='mt-2'>Description: A delightful place to enjoy authentic Indian cuisine.</p>
+        </section>
     </div>
   )
 }
