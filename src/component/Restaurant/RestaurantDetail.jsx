df01cd74349dfb183286acfb90f84c8f8b875282
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-31 15:36:38 +0530

    Auto commit: Modified src/component/Restaurant/RestaurantDetail.jsx

--- src/component/Restaurant/RestaurantDetail.jsx
+++ src/component/Restaurant/RestaurantDetail.jsx
@@ -30,7 +30,7 @@ const RestaurantDetailPage = () => {
     const [mainImage, setMainImage] = useState(restaurantImages[0]);
 
     return (
-        div className="font-sans">
+        <div>
         <div className="font-sans">
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <div className="flex flex-col lg:flex-row gap-12">
