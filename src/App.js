b39e01cd497a92489186f2bbcb2c58c0f098fc16
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-31 14:48:38 +0530

    Auto commit: Modified src/App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { darkTheme } from './Theme/DarkTheam';
 import Home from './component/Home/Home';
 import RestaurantDetail from './component/Restaurant/RestaurantDetail';
-
+imp
 function App() {
   return (
     <>
