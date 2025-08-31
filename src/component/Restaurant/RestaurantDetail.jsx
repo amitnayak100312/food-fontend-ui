d89cf52cff459dc3ce9278d9ad068dfe3a35232c
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-31 16:22:31 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantDetail.jsx
+++ src/component/Restaurant/RestaurantDetail.jsx
@@ -176,7 +176,7 @@ const RestaurantDetailPage = () => {
                                     </RadioGroup>
                                 </FormControl>
                               </div>
-                                            
+                                 <Divider />     
                               <div>
                                 <Typography variant="h5" sx={{paddingBottom:"1rem"}}>
                                   Food Category
