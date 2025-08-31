12504338b62da5f9f6d70573e8504b0af7592bb4
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-31 14:51:38 +0530

    Auto commit: Modified src/component/Restaurant/ProductPage.jsx

--- src/component/Restaurant/ProductPage.jsx
+++ src/component/Restaurant/ProductPage.jsx
@@ -23,7 +23,7 @@ const ProductPage = () => {
     ];
 
     return (
-        <div className="bg-white font-sans">
+        <div className="bg-wh font-sans">
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <div className="flex flex-col lg:flex-row gap-12">
                     
