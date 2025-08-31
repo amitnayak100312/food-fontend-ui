2df7adcd255a444521be113a50fc9805ab6ae1a4
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-31 14:36:37 +0530

    Auto commit: Modified src/component/Restaurant/RestaurantDetail.jsx

--- src/component/Restaurant/RestaurantDetail.jsx
+++ src/component/Restaurant/RestaurantDetail.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 
 const RestaurantDetail = () => {
     return (
-        <div className="px-5 py-5 lg:px-20">
-            <Grid container spacing={4} alignItems="center">
-                {/* Left Side: Image */}
-                <Grid item xs={12} md={6}>
+        <div className='px-5 py-5 lg:px-20'>
+            {/* SECTION FOR RESTAURANT INFO */}
+
+            {/* IMAGE GALLERY GRID */}
+            <Grid container spacing={2}>
+                {/* Main Image (Full Width) */}
+                <Grid item xs={12}>
                     <img
-                        className="w-full h-[40vh] object-cover rounded-md"
-                        src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                        alt="Restaurant Main"
-                    />
-                </Grid>
-                {/*
+                        className='w-full h-[40vh] object-cover rounded-md'
+                        src='https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                        alt='Restaurant Main'
                     />
                 </Grid>
 
@@ -42,7 +42,7 @@ const RestaurantDetail = () => {
                     />
                 </Grid>
             </Grid>
-            <section className='mb-5 mt-5 space-y-2'>
+            <section className='mb-5 mt-5'>
                 <h3 className='text-2xl font-semibold'>Restaurant Name</h3>
                 <p className='text-gray-500'>Cuisine: Indian</p>
                 <p className='mt-1 text-gray-500'>
