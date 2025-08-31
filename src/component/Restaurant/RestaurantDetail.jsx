189025f3c0bee6f9bb9273a0068376d7c287a4ca
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-31 15:59:30 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantDetail.jsx
+++ src/component/Restaurant/RestaurantDetail.jsx
@@ -25,6 +25,8 @@ const restaurantImages = [
     { id: 4, url: 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/335581096/original/7462bc5f9c9411f8e01ba3bf57f2115c69acc68d/create-high-quality-4k-renders-for-your-restaurant-and-cafe-e48e.jpg', alt: 'The exterior of the restaurant at night' },
 ];
 
+//Food 
+
 const RestaurantDetailPage = () => {
     // State for the image gallery
     const [mainImage, setMainImage] = useState(restaurantImages[0]);
