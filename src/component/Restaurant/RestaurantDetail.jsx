2781f3dd8f0c9e122b84005a95eca3c4f1b9803f
amitnayak100312 <amitnayak121003@gmail.com>
2025-08-31 15:46:30 +0530

    Updated Changes

--- src/component/Restaurant/RestaurantDetail.jsx
+++ src/component/Restaurant/RestaurantDetail.jsx
@@ -127,8 +127,8 @@ const RestaurantDetailPage = () => {
 
                              <div className="space-y-5 lg:sticky top-28">
                               <div>
-                                <Typography variant="h5" sx={}>
-                                  Menu
+                                <Typography variant="h5" sx={{paddingBottom:"1rem"}}>
+                                  Food Type
                                 </Typography>
                               </div>
                             </div>
