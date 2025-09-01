8abceb5fa2f2b7e01935b02a656c14c42e0ce3fa
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-01 22:48:18 +0530

    Auto commit: Modified src/component/Cart/Cart.jsx

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -75,9 +75,9 @@ const Cart = () => {
                             <Card className="flex gap-5 w-64 p-5">
                                 <AddLocationAltIcon />
                                 <div className='space-y-3 text-gray-500'>
-                                    <h1 className='font-semibold text-lg text-white'>Addd New Address</h1>  
-                                        <Button variant="outlined" fullWidth onClick={() => (handleOpenAddressModal)}>Add</Button>
-                                  
+                                    <h1 className='font-semibold text-lg text-white'>Addd New Address</h1>
+                                    <Button variant="outlined" fullWidth onClick={() => (handleOpenAddressModal)}>Add</Button>
+
                                 </div>
                             </Card>
                         </div>
