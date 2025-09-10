2401abaf91257b4453660575a34c0ec3dc725148
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-10 17:16:34 +0530

    Auto commit: Modified src/component/Profile/Profilenavigation.jsx

--- src/component/Profile/Profilenavigation.jsx
+++ src/component/Profile/Profilenavigation.jsx
@@ -3,6 +3,9 @@ import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import EventIcon from '@mui/icons-material/Event';
+import LogoutIcon from '@mui/icons-material/Logout';
 const menu =[
     {title:"Order History",icon:<ShoppingBagIcon/>},
     {title:"Favorites",icon:<FavoriteIcon/>},
