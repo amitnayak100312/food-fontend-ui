d49ebf10672b38ee24e15f013ba574956f138743
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-10 17:04:34 +0530

    Auto commit: Modified src/component/Profile/Profile.jsx, Modified src/component/Profile/Profilenavigation.jsx

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Profilenavigation from './Profilenavigation'
 const Profile = () => {
     return (
         <div>
             <div className='lg:flex justify-between'>
                 <div className='lg:flex justify-between'>
                     <div className='sticky h-[80vh] lg:w-[20%]'>
-                        <Profilenavigation>
+                        <Profilenavigation />
                     </div>
 
                 </div>
--- src/component/Profile/Profilenavigation.jsx
+++ src/component/Profile/Profilenavigation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Profilenavigation = () => {
   return (
     <div>
-      
+      <h2>Pro</h2>
     </div>
   )
 }
