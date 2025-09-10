15437fca78b52aae1013bd31ad28bca4f0b721c7
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-10 16:53:34 +0530

    Auto commit: Modified src/component/Profile/Profile.jsx

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <div>
       <div className='lg:flex justify-between'>
-        <div className='lg:w-[30%]'>User Information</div>
+        <div className='lg:flex justify-between'>User Information</div>
         <div className='lg:w-[70%]'>Order History</div>
       </div>
     </div>
