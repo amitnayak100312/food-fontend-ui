060fb3e0f5e7a1cd23cb58a2005debe6b5efa463
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-10 17:15:34 +0530

    Auto commit: Modified src/component/Profile/Profilenavigation.jsx

--- src/component/Profile/Profilenavigation.jsx
+++ src/component/Profile/Profilenavigation.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import HomeIcon from '@mui/icons-material/Home';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 const menu =[
     {title:"Order History",icon:<ShoppingBagIcon/>},
     {title:"Favorites",icon:<FavoriteIcon/>},
