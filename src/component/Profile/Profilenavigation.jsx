e570ca4c65c4bce09a687085e03330fb65abff35
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-12 14:04:15 +0530

    Auto commit: Modified src/component/Profile/Profilenavigation.jsx

--- src/component/Profile/Profilenavigation.jsx
+++ src/component/Profile/Profilenavigation.jsx
@@ -7,44 +7,44 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import EventIcon from '@mui/icons-material/Event';
 import LogoutIcon from '@mui/icons-material/Logout';
 import AddReaction from '@mui/icons-material/AddReaction';
-import { useMediaQuery,Drawer, Divider } from '@mui/material';
+import { useMediaQuery, Drawer, Divider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-const menu =[
-    {title:"Order History",icon:<ShoppingBagIcon/>},
-    {title:"Favorites",icon:<FavoriteIcon/>},
-    {title:"Address",icon:<AddReaction/>},
-    {title:"Home",icon:<HomeIcon/>},
-    {title:"Payment",icon:<AccountBalanceWalletIcon/>},
-    {title:"Notifications",icon:<NotificationsIcon/>},
-    {title:"Events",icon:<EventIcon/>},
-    {title:"Logout",icon:<LogoutIcon/>},
+const menu = [
+  { title: "Order History", icon: <ShoppingBagIcon /> },
+  { title: "Favorites", icon: <FavoriteIcon /> },
+  { title: "Address", icon: <AddReaction /> },
+  { title: "Home", icon: <HomeIcon /> },
+  { title: "Payment", icon: <AccountBalanceWalletIcon /> },
+  { title: "Notifications", icon: <NotificationsIcon /> },
+  { title: "Events", icon: <EventIcon /> },
+  { title: "Logout", icon: <LogoutIcon /> },
 ]
-const Profilenavigation = ({open,handleClose}) => {
-    const isSmallScreen = useMediaQuery('(max-width:1080px)');
+const Profilenavigation = ({ open, handleClose }) => {
+  const isSmallScreen = useMediaQuery('(max-width:1080px)');
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleNavigate=(item)=>{
-      navigate(`/my-profile/${item.title.toLowerCase()}`)
-    }
+  const handleNavigate = (item) => {
+    navigate(`/my-profile/${item.title.toLowerCase()}`)
+  }
   return (
     <div>
-      <Drawer variant={isSmallScreen ? "temporary" : "permanent"} 
-      onClose={handleClose} 
-      open ={isSmallScreen ? open : true} 
-      anchor="left"
-      sx={{zIndex:-1 ,position:'sticky' }}>
+      <Drawer variant={isSmallScreen ? "temporary" : "permanent"}
+        onClose={handleClose}
+        open={isSmallScreen ? open : true}
+        anchor="left"
+        sx={{ zIndex: -1, position: 'sticky' }}>
 
         <div className='w-[50vw] lg:w-[20vw] 
-        h-[100vh] flex flex-col justify-center text-xl gap-8 pt-16 '> 
-            {menu.map((item,i)=><>
-            <div onClick={() => handleNavigate(item)}   className='px-5 flex items-center space-x-5 cursor-pointer'>
+        h-[100vh] flex flex-col justify-center text-xl gap-8 pt-16 '>
+          {menu.map((item, i) => <>
+            <div onClick={() => handleNavigate(item)} className='px-5 flex items-center space-x-5 cursor-pointer'>
               {item.icon}
               <span>{item.title}</span>
             </div>
-            {i!== menu.length-1 && <Divider />}
-            </>)}
+            {i !== menu.length - 1 && <Divider />}
+          </>)}
         </div>
 
       </Drawer>
