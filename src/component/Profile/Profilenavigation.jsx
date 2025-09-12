cc82a141787f91f950b5d7f83a04eaa92358b69d
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-12 13:53:15 +0530

    Auto commit: Modified src/component/Profile/Profilenavigation.jsx

--- src/component/Profile/Profilenavigation.jsx
+++ src/component/Profile/Profilenavigation.jsx
@@ -36,7 +36,7 @@ const Profilenavigation = ({open,handleClose}) => {
       sx={{zIndex:1 ,position:'sticky' }}>
 
         <div className='w-[50vw] lg:w-[20vw] 
-        h-[100vh] flex flex-col justify-center text-xl gap-8 pt-16'> 
+        h-[100vh] flex flex-col justify-center text-xl gap-8 pt-20'> 
             {menu.map((item,i)=><>
             <div onClick={() => handleNavigate(item)}  className='px-5 flex items-center space-x-5 cursor-pointer'>
               {item.icon}
