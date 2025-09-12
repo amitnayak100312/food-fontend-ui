3f885b97e886003e769d2c655604b277c64351b9
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-12 14:09:15 +0530

    Auto commit: Modified src/component/Profile/Profile.jsx

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import Profilenavigation from './Profilenavigation'
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import UserProfile from './UserProfile';
+import { Favorite } from '@mui/icons-material';
 
 
 const Profile = () => {
@@ -17,14 +18,14 @@ const Profile = () => {
                     <div className='lg:w-[80%]'>
                     <Routes>
                         <Route path="/" element={<UserProfile />} />
-                        <Route path='/order history' element={<Orde} />
-                        <Route path='/favorites' element={<div>Favorites</div>} />
-                        <Route path='/address' element={<div>Address</div>} />
-                        <Route path='/home' element={<div>Home</div>} />
-                        <Route path='/payment' element={<div>Payment</div>} />
-                        <Route path='/notifications' element={<div>Notifications</div>} />
-                        <Route path='/events' element={<div>Events</div>} />
-                        <Route path='/logout' element={<div>Logout</div>} />
+                        <Route path='/order history' element={<Orders />} />
+                        <Route path='/favorites' element={<Favorite />} />
+                        <Route path='/address' element={<Address />} />
+                        <Route path='/home' element={<Home />} />
+                        <Route path='/payment' element={<Payment />} />
+                        <Route path='/notifications' element={<Notifications />} />
+                        <Route path='/events' element={<Events />} />
+                        <Route path='/logout' element={<Logout />} />
                     </Routes>
 
                     </div>
