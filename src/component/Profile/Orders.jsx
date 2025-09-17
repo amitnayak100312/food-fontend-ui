75183e0e2b5ea652fe12cbe6dc9e4d023db930a0
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:07:49 +0530

    Auto commit: Modified src/component/Profile/OrderCard.jsx, Modified src/component/Profile/Orders.jsx

--- src/component/Profile/Orders.jsx
+++ src/component/Profile/Orders.jsx
@@ -5,7 +5,9 @@ const Orders = () => {
     <div className='flex items-center flex-col'>
         <h1 className='text-2xl py-5 font-semibold text-center'>My Orders</h1>
         <div className='space-y-4 w-full lg:w-1/2'>
-         
+         {
+            [1,2,3,4,5].map((i)=></O>
+         }
     </div>
     </div>
   )
