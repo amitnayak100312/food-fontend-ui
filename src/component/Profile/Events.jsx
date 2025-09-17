600525e73e2bd8a7eb59dd00256f3f05b8da7506
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:23:20 +0530

    Auto commit: Added src/component/Profile/EventCard.jsx, Modified src/component/Profile/Events.jsx

--- src/component/Profile/Events.jsx
+++ src/component/Profile/Events.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Event = () => {
   return (
-    <div className=''>
+    <div className='mt-3 px-5 flex flex-wrap gap-5'>
+      {[1,1,1]}
         
     </div>
   )
