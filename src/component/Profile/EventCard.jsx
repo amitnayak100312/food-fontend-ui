39c28955e0f24790b1111700dd0cdbdba19d6210
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:41:56 +0530

    Auto commit: Modified src/component/Profile/EventCard.jsx, Modified src/component/Profile/Events.jsx

--- src/component/Profile/EventCard.jsx
+++ src/component/Profile/EventCard.jsx
@@ -6,8 +6,8 @@ const EventCard = () => {
     <div>
       <Card sx={{width:345}}>
         <CardMedia 
-        sx={{height:345}}  
-        image='https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg'/>
+        sx={{height:350}}  
+        image='https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg'/>
       <CardContent>
         <Typography variant="h5">
             Indian Fast Food
--- src/component/Profile/Events.jsx
+++ src/component/Profile/Events.jsx
@@ -4,6 +4,7 @@ import EventCard from './EventCard'
 const Event = () => {
   return (
     <div className='mt-3 px-5 flex flex-wrap gap-5'>
+      <h1 cla></h1>
       {[1,1,1].map((item)=><EventCard/>)}
         
     </div>
