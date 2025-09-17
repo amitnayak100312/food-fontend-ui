418b062442ca44356e3d8fe8f2a046619dda3cc4
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:38:20 +0530

    Auto commit: Modified src/component/Profile/EventCard.jsx

--- src/component/Profile/EventCard.jsx
+++ src/component/Profile/EventCard.jsx
@@ -7,7 +7,8 @@ const EventCard = () => {
       <Card sx={{width:345}}>
         <CardMedia 
         sx={{height:345}}  
-        image='https://www.pexels.com/photo/burger-with-fried-fries-on-black-plate-with-sauce-on-the-side-1199957/'/>
+        image='https://www.pexels.com/photo/
+        burger-with-fried-fries-on-black-plate-with-sauce-on-the-side-1199957/'/>
       <CardContent>
         <Typography variant="h5">
             Indian Fast Food
