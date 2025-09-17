49b25c0709512e72d350c4346f3e5808644a810b
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:29:20 +0530

    Auto commit: Modified src/component/Profile/EventCard.jsx

--- src/component/Profile/EventCard.jsx
+++ src/component/Profile/EventCard.jsx
@@ -10,8 +10,8 @@ const EventCard = () => {
         image='https://www.google.com/imgres?q=image%20food&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1551024601-bec78aea704b%3Ffm%3Djpg%26q%3D60%26w%3D3000%26ixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVsaWNpb3VzJTIwZm9vZHxlbnwwfHwwfHx8MA%253D%253D&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fdelicious-food&docid=jOprI7MUwRmUxM&tbnid=J3ELhHxz9_6meM&vet=12ahUKEwiDrPforuCPAxX6Z_UHHUxULXMQM3oECHoQAA..i&w=3000&h=3750&hcb=2&ved=2ahUKEwiDrPforuCPAxX6Z_UHHUxULXMQM3oECHoQAA'
       />
       <CardContent>
-        <Typography>
-            Indian 
+        <Typograph variant='h5' compoenet>
+            Indian Fast Food
         </Typography>
       </CardContent>
       </Card>
