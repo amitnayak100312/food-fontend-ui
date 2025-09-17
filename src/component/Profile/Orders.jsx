a988b6735316e4fda7b246c63cd95b39629725e9
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 22:57:50 +0530

    Auto commit: Modified src/component/Profile/Orders.jsx

--- src/component/Profile/Orders.jsx
+++ src/component/Profile/Orders.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Orders = () => {
   return (
-    <div>
-    <><h1>Orders</h1></>
+    <div className='flex items-center flex-col'>
+        <h1 className='text-2xl py-5 font-semibold text-center'>My Orders</h1>
+        <div className='space-y-4 w-full lg:w-1/2'>
+         
+    </div>
     </div>
   )
 }
