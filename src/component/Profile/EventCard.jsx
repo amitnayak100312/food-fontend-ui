98a0f17d576d89eb8af9bebce3a5e3845e0e23a8
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:24:20 +0530

    Auto commit: Modified src/component/Profile/EventCard.jsx

--- src/component/Profile/EventCard.jsx
+++ src/component/Profile/EventCard.jsx
@@ -1,1 +1,14 @@
-rafc
+import { Card, CardMedia } from '@mui/material'
+import React from 'react'
+
+const EventCard = () => {
+  return (
+    <div>
+      <Card>
+        <CardMedia image=''
+      </Card>
+    </div>
+  )
+}
+
+export default EventCard
