9fda3702a417d2e82fc09e1315114642cd7c80e2
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 22:42:50 +0530

    Auto commit: Modified src/component/Navbar/Navbar.jsx, Modified src/component/Profile/UserProfile.jsx

--- src/component/Profile/UserProfile.jsx
+++ src/component/Profile/UserProfile.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const UserProfile = () => {
   return (
-    <div>
-        UserProfile
+    <div className='min-h-[70vh] flex items-center justify-center text-center'>
+      <AccountCircleIcon sx={{ fontSize: '9rem', color: '#d10f50' }} />
+      <h1 className='py-5 text-3xl font-semibold'>User Profile</h1>
+      <p>Email : user@example.com</p>
+      <Button sx={{ margin:"2r"}}></Button>
     </div>
   )
 }
