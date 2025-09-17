0bc67fec550e9c7140766e723ef00bc4dce2052a
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-18 00:01:56 +0530

    Auto commit: Modified src/component/Profile/EventCard.jsx

--- src/component/Profile/EventCard.jsx
+++ src/component/Profile/EventCard.jsx
@@ -1,36 +1,40 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete';
 import React from 'react'
 
 const EventCard = () => {
-  return (
-    <div>
-      <Card sx={{width:345}}>
-        <CardMedia 
-        sx={{height:350}}  
-        image='https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg'/>
-      <CardContent>
-        <Typography variant="h5">
-            Indian Fast Food
-        </Typography>
+    return (
+        <div>
+            <Card sx={{ width: 345 }}>
+                <CardMedia
+                    sx={{ height: 350 }}
+                    image='https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg' />
+                <CardContent>
+                    <Typography variant="h5">
+                        Indian Fast Food
+                    </Typography>
 
-         <Typography variant="body2">
-            50% off on your first order
-        </Typography>
+                    <Typography variant="body2">
+                        50% off on your first order
+                    </Typography>
 
-        <div className='py-2 space-y-2'>
-            <p>{"mumbai"}</p>
-            <p className='text-sm text-blue-500'> February 14,2025 12:00 AM</p>
-            <p className='text-sm text-red-500'> February 15,2025 12:00 AM</p>
+                    <div className='py-2 space-y-2'>
+                        <p>{"mumbai"}</p>
+                        <p className='text-sm text-blue-500'> February 14,2025 12:00 AM</p>
+                        <p className='text-sm text-red-500'> February 15,2025 12:00 AM</p>
+                    </div>
+                </CardContent>
+
+                {
+                    <CardActions>
+                    <CardContent>
+                        <DeleteIcon />
+                    </CardContent>
+                </CardActions>}
+            </Card>
+
         </div>
-      </CardContent>
-    <C
-      <CardContent>
-        <DeleteIcon />
-      </CardContent>
-      </Card>
-    </div>
-  )
+    )
 }
 
 export default EventCard
