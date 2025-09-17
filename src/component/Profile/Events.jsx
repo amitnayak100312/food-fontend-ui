fad058625b0d8422d37c283981a04fcdd1a20d37
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:43:56 +0530

    Auto commit: Modified src/component/Profile/Events.jsx

--- src/component/Profile/Events.jsx
+++ src/component/Profile/Events.jsx
@@ -3,10 +3,14 @@ import EventCard from './EventCard'
 
 const Event = () => {
   return (
+    <div>
+      <h1 className='py-5 text-xl font-semibold text-center'>
+        Event List
+      </h1>
     <div className='mt-3 px-5 flex flex-wrap gap-5'>
-      <h1 className='flex '></h1>
       {[1,1,1].map((item)=><EventCard/>)}
         
+    </div>
     </div>
   )
 }
