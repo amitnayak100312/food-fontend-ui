e681c942c775d34d98851bc040518e3531da4f0f
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 22:47:49 +0530

    Auto commit: Modified src/component/Profile/UserProfile.jsx

--- src/component/Profile/UserProfile.jsx
+++ src/component/Profile/UserProfile.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Button } from '@mui/material';
+
 const UserProfile = () => {
+  const handleLogout = () => {}
   return (
     <div className='min-h-[70vh] flex items-center justify-center text-center'>
+      <div className='absolute top-0 left-0 w-full h-[30vh] bg-[#d10f50] -z-10'></div>
       <AccountCircleIcon sx={{ fontSize: '9rem', color: '#d10f50' }} />
       <h1 className='py-5 text-3xl font-semibold'>User Profile</h1>
       <p>Email : user@example.com</p>
-      <Button sx={{ margin:"2r"}}></Button>
+      <Button onClick={handleLogout} sx={{ margin:"2rem 0rem"}}>Logout</Button>
     </div>
   )
 }
