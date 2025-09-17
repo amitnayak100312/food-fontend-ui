84eed7b1f9c61831a4a7d2fe75e647cf1402aff1
amitnayak100312 <amitnayak121003@gmail.com>
2025-09-17 23:19:20 +0530

    Auto commit: Modified src/component/Profile/Favorites.jsx

--- src/component/Profile/Favorites.jsx
+++ src/component/Profile/Favorites.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Favorites = () => {
   return (
     <div className=''>
-      <h1 className='py-5 text-xl font-semibold text-center'></h1>
+      <h1 className='py-5 text-xl font-semibold text-center'>
+        My Favorites
+      </h1>
+
+      <div className='flex flex-wrap ju'>
+
+      </div>
     </div>
   )
 }
